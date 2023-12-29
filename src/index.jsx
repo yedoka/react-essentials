7a2705a81ac1ea77420b563a7693fc6b57327a98
yedoka <akedilbekbolatov@gmail.com>
2023-12-29 11:41:53 +0100

    alternative way to create App

--- src/index.jsx
+++ src/index.jsx
@@ -5,5 +5,5 @@ import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-// ReactDOM.createRoot(entryPoint).render(<App />);
-ReactDOM.createRoot(entryPoint).render(React.createElement(App));
+ReactDOM.createRoot(entryPoint).render(<App />);
+// ReactDOM.createRoot(entryPoint).render(React.createElement(App));
